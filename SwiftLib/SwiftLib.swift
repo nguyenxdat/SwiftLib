a5d91704926d336267f82cbc6f3eeb7272f504f6
datnx <datnx@gtech.ai>
2022-07-19 09:20:13 +0700

    update lib

---
+++ SwiftLib/SwiftLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftLib.swift
+//  SwiftLib
+//
+//  Created by Nguyen Dat on 19/07/2022.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
