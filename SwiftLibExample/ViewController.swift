89cef1e1d174cd1f9590e17306474368c10150d4
datnx <datnx@gtech.ai>
2022-07-19 09:15:48 +0700

    update swift lib example

---
+++ SwiftLibExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftLibExample
+//
+//  Created by Nguyen Dat on 19/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
